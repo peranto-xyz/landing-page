95146d897c1f14ff9ebcd55919e2bc8fe9bc34a3
Metabaron <axnotliztac@gmail.com>
2023-10-17 22:45:49 -0600

    init

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>| Peranto |</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Peranto description" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
